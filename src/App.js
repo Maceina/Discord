004cba41257c11b1c8fd34aee5b2a1881a7822dc
Your Name <you@example.com>
2020-11-03 16:54:01 +0200

    Sidebar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import Sidebar from './Sidebar'
+
 
 function App() {
   return (
     <div className="app">
-      <h1>Let's build discrod clone!</h1>
+      <Sidebar/>
     </div>
   );
 }
