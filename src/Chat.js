8f57925c07c2d46688360e8d350c96c9929edc61
Your Name <you@example.com>
2020-11-05 14:49:19 +0200

    dummy messages

--- src/Chat.js
+++ src/Chat.js
@@ -5,13 +5,16 @@ import AddCircleIcon from '@material-ui/icons/AddCircle';
 import CardGiftcardIcon from '@material-ui/icons/CardGiftcard';
 import GifIcon from '@material-ui/icons/Gif';
 import EmojiEmotionsIcon from '@material-ui/icons/EmojiEmotions';
+import Message from './Message';
 
 function Chat() {
     return (
         <div className="chat">
             <ChatHeader/>
             <div className="chat__messages">
-
+            <Message/>
+            <Message/>
+            <Message/>
             </div>
             <div className="chat__input">
                 <AddCircleIcon fontSize="large"/>
